2d0d8b40e37bd634fcc5b8e5031a3a054bd91d50
j0s3dr4ft <162845697+j0s3dr4ft@users.noreply.github.com>
2024-03-09 19:41:44 -0500

    Add files via upload

---
+++ IMC.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Ingrese su Peso: ");
+double masaCorporal = double.Parse(Console.ReadLine());
+
+//se usa coma para los decimales (1,2 en lugar de 1.2)
+Console.WriteLine("Ingrese su Estatura con coma: ");
+double altura = double.Parse(Console.ReadLine());
+
+
+double imc = ( masaCorporal / (altura * altura));
+Console.WriteLine("Indice de Masa Corporal: " + imc);
+
+if (imc > 25)
+{
+    Console.WriteLine("Sobrepeso ");
+}
+else if (imc > 18.5) {
+    Console.WriteLine("Peso normal ");
+}
+else if (imc < 18.5)
+{
+    Console.WriteLine("Bajo de Peso");
+}
+
+
+
+
+
+
+
+
