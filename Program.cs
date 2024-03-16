2524892031dc0730009a386b90ab1998a413637f
Jose Alvarez <162845697+j0s3dr4ft@users.noreply.github.com>
2024-03-16 14:34:56 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Programa para saber su Tarifa (Comfama");
+
+Console.WriteLine("Ingrese su Salario: ");
+double salario = double.Parse(Console.ReadLine());
+
+
+if (salario < 2600000)
+{
+    Console.WriteLine("Eres Tarifa A");
+}
+else if(salario < 5200000)
+{
+    Console.WriteLine("Eres Tarifa B");
+}
+else 
+{
+    Console.WriteLine("Eres Tarfia C");
+}
+
